b36befeeabe92bd03f40353f45ad1a1193729559
JonathanGuillenL <jonathanguillenlainez22@gmail.com>
2025-07-12 17:03:05 -0600

    Agregar inicializador de base de datos import.sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO TipoCliente(id, descripcion) values (1, 'Cliente espontaneo');
+INSERT INTO TipoCliente(id, descripcion) values (2, 'Medico afiliado');
+INSERT INTO TipoCliente(id, descripcion) values (3, 'Clinica afiliada');
